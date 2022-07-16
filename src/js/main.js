6461fea0e0ae2b87d7c33731b649b4ab69ced445
Olga RG <olga_1847@hotmail.com>
2022-07-16 14:27:40 +0200

    Add favorites section with all interactions

--- src/js/main.js
+++ src/js/main.js
@@ -17,31 +17,92 @@ let favorites = [];
 
 const renderSeries = (arraySeries) => {
     let html = '';
+    
 
     for (const oneSerie of arraySeries) {
-        html += `<li class="" id="${oneSerie.mal_id}">`;
+
+        html += `<li class="js-serie" id="${oneSerie.mal_id}">`;
         html += `<h2>${oneSerie.title}</h2>`;
+        if (oneSerie.images.jpg.image_url === "https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png"){
+            oneSerie.images.jpg.image_url = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+            html += `<img src="${oneSerie.images.jpg.image_url}" />`;
+        } else {
         html += `<img src="${oneSerie.images.jpg.image_url}" />`;
+         }
         html += `</li>`;
-
     }
-    listSearch.innerHTML = html;
-}
+    return html;
+};
 
+ const renderSeriesSearch = () => {
+    let html= renderSeries(series);
+    listSearch.innerHTML = html;
+    listenerSerie()
+ }
+
+ const renderSeriesFavorites = () => { 
+    let html = renderSeries(favorites);
+    listFavorites.innerHTML = html;
+    listenerSerie();
+    console.log(favorites);
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+ }
 
 
 const handleSearch = (event) => {
     event.preventDefault();
-
     const inputValue = inputSearch.value.toLowerCase();
-
-    fetch (`https://api.jikan.moe/v4/anime?q=${inputValue}`)
-        .then((response) => response.json())
-        .then ((data) => {
-            series = data.data;
-            renderSeries(series);
-        })
+    getDataAPI(inputValue);
 }
 
 btnSearch.addEventListener('click', handleSearch);
 inputSearch.addEventListener('keyup', handleSearch);
+
+const getDataAPI = (inputValue) => {
+    fetch (`https://api.jikan.moe/v4/anime?q=${inputValue}`)
+        .then((response) => response.json())
+        .then ((data) => {
+            series = data.data;
+            renderSeriesSearch();
+            
+    })
+}
+
+const handleClick = (event) => {
+    const idSelected = parseInt(event.currentTarget.id);
+    const serieFound = series.find((oneSerie) => oneSerie.mal_id === idSelected);
+   
+    const favoriteFound = favorites.findIndex((fav) => fav.mal_id === idSelected);
+
+    favorites = dataLS;
+   
+    if (favoriteFound === -1){
+        favorites.push(serieFound);
+    } else {
+        favorites.splice(favoriteFound, 1);
+    }
+    renderSeriesFavorites();
+    
+}
+
+function listenerSerie() {
+    const liSeries = document.querySelectorAll('.js-serie');
+    for (const li of liSeries) {
+        li.addEventListener('click', handleClick);
+    }
+}
+
+const dataLS = JSON.parse(localStorage.getItem('favorites'));
+
+function onLoad() {
+    
+    if (dataLS !== '') {
+        let html = renderSeries(dataLS);
+        listFavorites.innerHTML = html;
+    } else {
+        listFavorites.innerHTML = '';
+    }
+    
+}
+
+onLoad();
